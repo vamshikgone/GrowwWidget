0efdd9580e6602614173fa6bbe5e1da36499d64b
Vamshi Krishna Reddy Gone <vk.gone18@gmail.com>
2024-12-28 14:11:25 -0600

    Initial Commit

---
+++ GrowwWidget/GrowwWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowwWidgetApp.swift
+//  GrowwWidget
+//
+//  Created by Savitha Reddy on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct GrowwWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GrowwWidgetTests/GrowwWidgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  GrowwWidgetTests.swift
+//  GrowwWidgetTests
+//
+//  Created by Savitha Reddy on 12/28/24.
+//
+
+import Testing
+@testable import GrowwWidget
+
+struct GrowwWidgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
